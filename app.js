c3878ddfa5d1309742607c34d3f1f59c758c7491
chibanimaher <chibani_maher92@hotmail.fr>
2017-09-13 23:40:02 +0100

    add data table to index.html

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-var  App= angular.module('weatherApp',[]);
-App.controller('weatherCtrl', function ($scope, $http){
-
+var App = angular.module('petrolApp',[]);
+App.controller('exportCtrl', function ($scope, $http){
   $http({
-
-    method: 'Get',
+    method: 'GET',
     url:'http://catalog.industrie.gov.tn/api/action/datastore_search?resource_id=5ad54062-e18e-4a54-965a-ec1642412efa&limit=10'
   }).success(function(data) {
-    $scope.objects = data ;
-
+    if (data.success){
+      $scope.statItems = data.result.records;
+    }  
   }).catch(function(response){
     console.log('response:',response);
   });
-}
-);
+}); 
 
       
 
