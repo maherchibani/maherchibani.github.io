d8584b88a85d980a0e01db7aa0f7e3bcd622ddaf
chibanimaher <chibani_maher92@hotmail.fr>
2017-09-16 15:34:11 +0100

    add graf to index.html

--- public/app.js
+++ public/app.js
@@ -2,37 +2,46 @@ var App = angular.module('petrolApp',["ng-fusioncharts"]);
   
 App.controller('exportCtrl', function ($scope, $http, $filter){
    $scope.dataSource = {};
-    var init = function(){
+   $scope.year ="2017";
+   var init = function(){
         $http({
-        method: 'GET',
-        url:'http://catalog.industrie.gov.tn/api/action/datastore_search?resource_id=5ad54062-e18e-4a54-965a-ec1642412efa&limit=12'
+        method : 'POST',
+        url : 'http://catalog.industrie.gov.tn/api/action/datastore_search',
+        data : {
+            resource_id:"5ad54062-e18e-4a54-965a-ec1642412efa",
+            filters:{ "Year":$scope.year },
+            limit:1000,
+            offset:0
+        },
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
         }).success(function(data) {
             if (data.success){
                 $scope.statItems = data.result.records;
                 $scope.dataSource = adaptData();
-
             }  
         }).catch(function(response){
             console.log('response:',response);            
         });
     }
-
+    $scope.changeValue = function(){
+        init();
+    }
+    
     var adaptData = function(){
         var dataSource = {
                 chart: {
-                    caption: "Exportation mensuelle du pétrole brut en Tunisie ",
+                    caption: "Monthly export of crude oil in Tunisia "+$scope.year,
                     subCaption: "",
                     numberPrefix: "kt",
                     theme: "ocean"
                 },
-                 data :[]
+                data :[]
              };
-
-            for ( var i = 0; i < $scope.statItems.length ; i++) {
-                dataSource.data.push({
-                    label: $scope.statItems[i].Date,
+        for ( var i = 0; i < $scope.statItems.length ; i++) {
+            dataSource.data.push({
+                label: $scope.statItems[i].Date,
                 value: ""+ $filter('number') ($scope.statItems[i]['Exportation Pétrole brut en kt'],2)
-                });
+            });
         }
         return dataSource;
     }
